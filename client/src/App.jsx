7b8fcf93439ee1de2041e0fc0dd11f2abfab2894
Shubham-404 <shubh.shubhamkrsingh@gmail.com>
2025-09-28 14:16:37 +0530

    add client side with react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,9 @@
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import Layout from "./Layout";
-import Dashboard from "./pages/Dashboard";
-import Profile from "./pages/Profile";
-import Portfolio from "./pages/Portfolio";
-import Academics from "./pages/Academics";
-import Competitions from "./pages/Competitions";
-import Certifications from "./pages/Certifications";
-import Volunteering from "./pages/Volunteering";
-import Analytics from "./pages/Analytics";
-import Accreditation from "./pages/Accreditation";
-import Settings from "./pages/Settings";
-import Activities from "./pages/Activities";
-import Onboarding from "./pages/Onboarding";
+import React from 'react'
 
-export default function App() {
+const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Navigate to="/dashboard" replace />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="portfolio" element={<Portfolio />} />
-        <Route path="academics" element={<Academics />} />
-        <Route path="competitions" element={<Competitions />} />
-        <Route path="certifications" element={<Certifications />} />
-        <Route path="volunteering" element={<Volunteering />} />
-        <Route path="analytics" element={<Analytics />} />
-        <Route path="accreditation" element={<Accreditation />} />
-        <Route path="activities" element={<Activities />} />
-        <Route path="settings" element={<Settings />} />
-      </Route>
-      <Route path="/onboarding" element={<Onboarding />} />
-    </Routes>
-  );
+    <div className='text-red-300'>App</div>
+  )
 }
+
+export default App
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite"
-import jsconfigPaths from "vite-jsconfig-paths"
-import path from 'path';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
   plugins: [
     react(),
-    tailwindcss(),
-    jsconfigPaths()
+    tailwindcss()
   ],
 })
